20751ce256fa7ee803507089e1ef01632fd4c9d8
ddieffen <ddieffen@gmail.com>
2015-10-06 11:44:28 -0500

    Updated unit handling and object reference error
    Now works much better than before and can output results in text files
    for further handling

--- 2015/Tools/CalculatorBatch/CalculatorBatch/Program.cs
+++ 2015/Tools/CalculatorBatch/CalculatorBatch/Program.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Greet.UnitLib3;
-using Greet.DataStructureV4;
-using Greet.Model;
-using Greet.DataStructureV4.Entities;
-using Greet.DataStructureV4.Interfaces;
-using Greet.DataStructureV4.ResultsStorage;
 using System.Data;
 using System.Threading;
 using System.Reflection;
+using Greet.DataStructureV4.ResultsStorage;
+using Greet.DataStructureV4.Entities;
+using Greet.DataStructureV4;
+using Greet.DataStructureV4.Interfaces;
+using Greet.Model;
+using Greet.UnitLib3;
+using Greet.ConvenienceLib;
 
 namespace CalculatorBatch
 {
@@ -25,12 +26,16 @@ namespace CalculatorBatch
     /// </summary>
     class Program
     {
+        /// <summary>
+        /// Program entry point
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Parsing given parameters...");
             //Using the filename provided as the first argument
             string fileName = args[0];
-            
+
             //Using the years defined in the second argument
             string years = args[1];
             string[] yearsSplit = years.Split(',');
@@ -73,8 +78,23 @@ namespace CalculatorBatch
             GProject project = new GProject();
             project.Load(fileName);
 
+            //Assign main output resource IDs to all the inputsResourceReferences
+            foreach (InputResourceReference iref in inRef)
+            {
+                if (iref.SourceType == Greet.DataStructureV4.Interfaces.Enumerators.SourceType.Pathway)
+                {
+                    if (project.Dataset.PathwaysData.ContainsKey(iref.SourceMixOrPathwayID))
+                        iref.ResourceId = project.Dataset.PathwaysData[iref.SourceMixOrPathwayID].MainOutputResourceID;
+                }
+                else if (iref.SourceType == Greet.DataStructureV4.Interfaces.Enumerators.SourceType.Mix)
+                {
+                    if (project.Dataset.MixesData.ContainsKey(iref.SourceMixOrPathwayID))
+                        iref.ResourceId = project.Dataset.MixesData[iref.SourceMixOrPathwayID].MainOutputResourceID;
+                }
+            }
+
             //Creating a new instance of a dictionary used to store results of the simulations
-            Dictionary<InputResourceReference, Dictionary<int, Results>> savedResults = new Dictionary<InputResourceReference, Dictionary<int, Results>>();   
+            Dictionary<InputResourceReference, Dictionary<int, Results>> savedResults = new Dictionary<InputResourceReference, Dictionary<int, Results>>();
 
             //Running simulations for every provided years
             foreach (int simulationYear in yearsList)
@@ -83,26 +103,26 @@ namespace CalculatorBatch
                 //Set the current year for simulations
                 BasicParameters.SelectedYear = project.Dataset.ParametersData.CreateUnregisteredParameter(project.Dataset, "", simulationYear);
                 Calculator calc = new Calculator();
-               
+
                 //Run the simulations for the loaded project and defined year, we need to wait completion as the RunCalculationMethod is Async
                 var manualEvent = new ManualResetEvent(false);
                 calc.CalculationDoneEvent += () => manualEvent.Set();
                 calc.RunCalculations(project);
                 manualEvent.WaitOne();
-             
+
                 //Loop over all the pathways and mixes ID that we wish to save
                 foreach (InputResourceReference pathMixToSave in inRef)
                 {
                     if (!savedResults.ContainsKey(pathMixToSave))
                         savedResults.Add(pathMixToSave, new Dictionary<int, Results>());
-                    if(!savedResults[pathMixToSave].ContainsKey(simulationYear))
+                    if (!savedResults[pathMixToSave].ContainsKey(simulationYear))
                     {
                         //Pull the results and add them to the dictionary used to store results
                         Results results;
-                        if(pathMixToSave.SourceType == Enumerators.SourceType.Pathway)
-                            results = project.Dataset.PathwaysData[pathMixToSave.SourceMixOrPathwayID].getMainOutputResults().Results;
+                        if (pathMixToSave.SourceType == Enumerators.SourceType.Pathway)
+                            results = Convenience.Clone(project.Dataset.PathwaysData[pathMixToSave.SourceMixOrPathwayID].getMainOutputResults().Results);
                         else
-                            results = project.Dataset.MixesData[pathMixToSave.SourceMixOrPathwayID].getMainOutputResults().Results;
+                            results = Convenience.Clone(project.Dataset.MixesData[pathMixToSave.SourceMixOrPathwayID].getMainOutputResults().Results);
                         savedResults[pathMixToSave].Add(simulationYear, results);
                     }
                 }
@@ -110,6 +130,9 @@ namespace CalculatorBatch
 
             //Export all the desired results to an Excel spreadsheet
             Console.WriteLine("Export all selected results...");
+            string preferedMass = "g";
+            string preferedEnergy = "Btu";
+            string preferedVolume = "gal";
 
             foreach (KeyValuePair<InputResourceReference, Dictionary<int, Results>> pair in savedResults)
             {
@@ -122,7 +145,6 @@ namespace CalculatorBatch
                 dt.Columns.Add("Items");
                 foreach (int simulationYear in pair.Value.Keys)
                     dt.Columns.Add(simulationYear.ToString());
-
                 List<string> rowString = new List<string>();
                 foreach (string resGrp in resGroups)
                 {
@@ -130,21 +152,28 @@ namespace CalculatorBatch
                     rowString.Add(resGrp);
                     foreach (int simulationYear in pair.Value.Keys)
                     {
+                        Results results = pair.Value[simulationYear];
+                        double amountRatio = GetFunctionalRatio(project.Dataset, results, pair.Key.ResourceId);
+
                         if (resGrp == "Total Energy")
                         {
-                            LightValue totalE = pair.Value[simulationYear].wellToProductEnem.materialsAmounts.TotalEnergy();
-                            rowString.Add(totalE.ToString());
+                            LightValue totalE = results.wellToProductEnem.materialsAmounts.TotalEnergy();
+                            rowString.Add(NiceValueWithAttribute(totalE * amountRatio, preferedEnergy));
                         }
                         else
                         {
-                            Dictionary<int, IValue> resGroupes = pair.Value[simulationYear].WellToProductResourcesGroups(project.Dataset);
+                            Dictionary<int, IValue> resGroupes = results.WellToProductResourcesGroups(project.Dataset);
                             int resGrpId = project.Dataset.ResourcesData.Groups.Values.Single(item => item.Name == resGrp).Id;
-                            rowString.Add(resGroupes[resGrpId].ToString());
+                            LightValue groupValue = new LightValue(resGroupes[resGrpId].Value, resGroupes[resGrpId].UnitExpression);
+                            if (groupValue.Dim == DimensionUtils.ENERGY)
+                                rowString.Add(NiceValueWithAttribute(groupValue * amountRatio, preferedEnergy));
+                            else
+                                rowString.Add(NiceValueWithAttribute(groupValue * amountRatio, preferedVolume));
                         }
                     }
                     dt.Rows.Add(rowString.ToArray());
                 }
-                
+
 
                 foreach (string poll in pollutants)
                 {
@@ -152,26 +181,34 @@ namespace CalculatorBatch
                     rowString.Add(poll);
                     foreach (int simulationYear in pair.Value.Keys)
                     {
+                        Results results = pair.Value[simulationYear];
+                        double amountRatio = GetFunctionalRatio(project.Dataset, results, pair.Key.ResourceId);
+
                         int polId = project.Dataset.GasesData.Values.Single(item => item.Name == poll).Id;
-                        rowString.Add(pair.Value[simulationYear].wellToProductEnem.emissions[polId].ToString());
+                        rowString.Add(NiceValueWithAttribute(
+                            new LightValue(results.wellToProductEnem.emissions[polId], DimensionUtils.MASS) * amountRatio
+                            , preferedMass));
                     }
                     dt.Rows.Add(rowString.ToArray());
                 }
-                
-                
+
+
                 foreach (string resGrp in polGroups)
                 {
                     rowString = new List<string>();
                     rowString.Add(resGrp);
                     foreach (int simulationYear in pair.Value.Keys)
                     {
+                        Results results = pair.Value[simulationYear];
+                        double amountRatio = GetFunctionalRatio(project.Dataset, results, pair.Key.ResourceId);
+
                         Dictionary<int, IValue> emGroupes = pair.Value[simulationYear].WellToProductEmissionsGroups(project.Dataset);
                         int grpId = project.Dataset.GasesData.Groups.Values.Single(item => item.Name == resGrp).Id;
-                        rowString.Add(emGroupes[grpId].ToString());
+                        rowString.Add(NiceValueWithAttribute(new LightValue(emGroupes[grpId].Value, emGroupes[grpId].UnitExpression) * amountRatio, preferedMass));
                     }
                     dt.Rows.Add(rowString.ToArray());
                 }
-                
+
 
                 foreach (string poll in pollutants)
                 {
@@ -179,18 +216,26 @@ namespace CalculatorBatch
                     rowString.Add(poll);
                     foreach (int simulationYear in pair.Value.Keys)
                     {
+                        Results results = pair.Value[simulationYear];
+                        double amountRatio = GetFunctionalRatio(project.Dataset, results, pair.Key.ResourceId);
+
                         int polId = project.Dataset.GasesData.Values.Single(item => item.Name == poll).Id;
-                        rowString.Add(pair.Value[simulationYear].wellToProductUrbanEmission[polId].ToString());
+                        rowString.Add(NiceValueWithAttribute(new LightValue(results.wellToProductUrbanEmission[polId], DimensionUtils.MASS) * amountRatio, preferedMass));
                     }
                     dt.Rows.Add(rowString.ToArray());
                 }
-                
+
 
                 string value = ConvertDataTableToString(dt);
                 System.IO.File.WriteAllText("Results-" + pair.Key.SourceType.ToString() + "-" + pair.Key.SourceMixOrPathwayID.ToString() + ".txt", value);
             }
         }
 
+        /// <summary>
+        /// Creates a human readable string representing a data table
+        /// </summary>
+        /// <param name="dataTable">Data table to be converted as a string</param>
+        /// <returns>String representing the data table</returns>
         public static string ConvertDataTableToString(DataTable dataTable)
         {
             var output = new StringBuilder();
@@ -215,6 +260,110 @@ namespace CalculatorBatch
             }
             string fullString = output.ToString();
             return fullString.Replace("\t\n", Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Returns the coefficient to be used in order to display the results according to the preferences of the IResult
+        /// object used to create the instance of this object. Returns the prefered functional unit divided by the functional unit.
+        /// 
+        /// In case of the FunctionalUnit is null or the PreferedUnit is null, this method returns a ratio of 1. This is usefull to
+        /// display the Inputs results and the TransportationSteps results which are accounted for all outputs and not a specific one.
+        /// In these cases instead of defining a PreferedUnit for each output we prefer to define none to make things simpler (see InputResult.GetResults)
+        /// 
+        /// Before display all results must be multiplied by this coefficient
+        /// 
+        /// May return a NullReferenceExeption if the IResult object does not define FunctinoalUnit, PreferedDisplayedUnit or PreferedDisplayedAmount
+        /// </summary>
+        /// <param name="results"></param>
+        /// <returns></returns>
+        internal static double GetFunctionalRatio(GData data, Results results, int producedResourceId)
+        {
+            if (results != null && results.CustomFunctionalUnitPreference != null && results.CustomFunctionalUnitPreference.PreferredUnitExpression != null)
+            {
+                LightValue functionalUnit = new LightValue(1.0, results.BottomDim);
+                LightValue preferedFunctionalUnit = GetPreferedVisualizationFunctionalUnit(data, results, producedResourceId);
+
+                switch (preferedFunctionalUnit.Dim)
+                {
+                    case DimensionUtils.MASS: // HARDCODED
+                        {
+                            functionalUnit = data.ResourcesData[producedResourceId].ConvertToMass(functionalUnit);
+                        } break;
+                    case DimensionUtils.ENERGY: // HARDCODED
+                        {
+                            functionalUnit = data.ResourcesData[producedResourceId].ConvertToEnergy(functionalUnit);
+                        } break;
+                    case DimensionUtils.VOLUME: // HARDCODED
+                        {
+                            functionalUnit = data.ResourcesData[producedResourceId].ConvertToVolume(functionalUnit);
+                        } break;
+                }
+                return preferedFunctionalUnit.Value / functionalUnit.Value;
+            }
+            else
+                return 1;
+        }
+
+        /// <summary>
+        /// Returns the functional unit to be used on display: User prefered, default million btu or functional unit of the
+        /// process in case of the database does not contains enough information to convert to one million btu
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="results"></param>
+        /// <param name="producedResourceId"></param>
+        /// <returns></returns>
+        internal static LightValue GetPreferedVisualizationFunctionalUnit(GData data, Results results, int producedResourceId)
+        {
+            LightValue preferedFunctionalUnit;
+            if (results.CustomFunctionalUnitPreference.enabled)
+                preferedFunctionalUnit = new LightValue(results.CustomFunctionalUnitPreference.Amount, results.CustomFunctionalUnitPreference.PreferredUnitExpression);
+            else if (data.ResourcesData[producedResourceId].CanConvertTo(DimensionUtils.ENERGY, new LightValue(1.0, results.BottomDim)))
+                preferedFunctionalUnit = new LightValue(1, "MJ");
+            else
+                preferedFunctionalUnit = new LightValue(1.0, results.BottomDim);
+            return preferedFunctionalUnit;
+        }
+
+        /// <summary>
+        /// Returns the functional unit to be used on display: User prefered, default million btu or functional unit of the
+        /// process in case of the database does not contains enough information to convert to one million btu
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="results"></param>
+        /// <param name="producedResourceId"></param>
+        /// <returns></returns>
+        internal static string GetPreferedVisualizationFunctionalUnitString(GData data, Results results, int producedResourceId)
+        {
+            string preferedFunctionalUnit = "";
+            try
+            {
+                if (results.CustomFunctionalUnitPreference.enabled)
+                    preferedFunctionalUnit = results.CustomFunctionalUnitPreference.Amount.ToString() + " " + results.CustomFunctionalUnitPreference.PreferredUnitExpression;
+                else if (data.ResourcesData[producedResourceId].CanConvertTo(DimensionUtils.ENERGY, new LightValue(1.0, results.BottomDim)))
+                    preferedFunctionalUnit = "1 MJ";
+                else
+                {
+                    AQuantity qty = Units.QuantityList.ByDim(results.BottomDim);
+                    preferedFunctionalUnit = "1 " + qty.SiUnit.Expression;
+                }
+            }
+            catch { }
+
+            return preferedFunctionalUnit;
+        }
+
+        internal static string NiceValueWithAttribute(LightValue value, string preferedExpression = "")
+        {
+            double automaticScalingSlope = 1;
+            string overrideUnitAttribute = "";
+            return GuiUtils.FormatSIValue(value.Value
+                , 0
+                , out overrideUnitAttribute
+                , out automaticScalingSlope
+                , false
+                , 4
+                , value.Dim
+                , preferedExpression) + " " + overrideUnitAttribute;
         }
     }
 }
